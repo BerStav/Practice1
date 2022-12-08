c284680c97e61b209eb91a3397fdbb212df1308c
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-09 01:58:58 +0700

    Taks3 done

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+Console.Write("We'll tell you if your number is even, please, enter it = ");
+int a = int.Parse(Console.ReadLine());
+
+int x = a % 2;
+
+if (x == 0)
+{
+    Console.Write("Yes");
+}
+
+else
+{
+    Console.Write("No");
+}
