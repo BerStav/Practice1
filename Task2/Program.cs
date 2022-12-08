4d14f99f6fb0edf705cb99c25e7f3d10edae117c
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-09 01:58:38 +0700

    Taks2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+Console.Write("Please, enter the first number = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Please, enter the second number = ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Please, enter the third number = ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > a) max = b;
+
+if (c > b) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
