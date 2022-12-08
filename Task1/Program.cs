cc19a40e7df0f6492bf4b1307c49f53cd336fdc4
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-09 00:39:31 +0700

    Taks1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая принимает на вход два числа и выдает, какое число большее, а какое меньшее
+
+Console.Write("Please, enter the first number = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Please, enter the second number = ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+
+else
+{
+    Console.Write("max = ");
+    Console.Write(b); 
+}
